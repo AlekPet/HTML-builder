f2c8fde284060208eadab377ae6e5df458d9c84a
Aleksey Petrov (AlekPet) <alexepetrof@gmail.com>
2024-01-18 18:24:32 +0300

    feat: implement task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const secretFolder = path.join(__dirname, 'secret-folder');
+
+fs.readdir(
+  secretFolder,
+  {
+    withFileTypes: true,
+  },
+  (err, files) => {
+    if (err) throw err;
+
+    files.forEach((file) => {
+      if (file.isDirectory()) return;
+      const pathFile = path.resolve(secretFolder, file.name);
+
+      const ext = path.extname(pathFile).slice(1);
+      const filename = path.parse(pathFile).name;
+
+      fs.stat(pathFile, (err, stats) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+
+        console.log(`${filename} - ${ext} - ${stats.size}`);
+      });
+    });
+  },
+);
